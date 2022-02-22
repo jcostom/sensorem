e26c117609de3bcae7b256392024d34ddedef13f
Jason Costomiris <jcostom@jasons.org>
2022-02-22 13:25:54 -0500

    add gitignore and cleanup from flake8 intro

--- sensorem.py
+++ sensorem.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import os
-import json
 import time
 import requests
 from influxdb_client import InfluxDBClient
@@ -16,25 +15,32 @@ influxToken = os.getenv('influxToken')
 influxURL = os.getenv('influxURL')
 influxMeasurementName = os.getenv('influxMeasurementName')
 
+
 def c2f(celsius):
     return (celsius * 9/5) + 32
+
 
 def readSensor(sbURL, sbHeaders):
     r = requests.get(sbURL, headers=sbHeaders)
     # return array of (degF, rHum)
-    return (round(c2f(r.json()['body']['temperature']),1), r.json()['body']['humidity'])
+    return (round(c2f(r.json()['body']['temperature']), 1),
+            r.json()['body']['humidity'])
+
 
 def writeLogEntry(message, status):
-    print(time.strftime("[%d %b %Y %H:%M:%S %Z]", time.localtime()) + " {}: {}".format(message, status))
+    print(time.strftime("[%d %b %Y %H:%M:%S %Z]", time.localtime())
+          + " {}: {}".format(message, status))
+
 
 def main():
     url = "/".join(
         ("https://api.switch-bot.com/v1.0/devices",
-        DEVID,
-        "status")
+         DEVID,
+         "status")
     )
-    headers = { 'Authorization': APIKEY }
-    influxClient = InfluxDBClient(url=influxURL,token=influxToken,org=influxOrg)
+    headers = {'Authorization': APIKEY}
+    influxClient = InfluxDBClient(url=influxURL, token=influxToken,
+                                  org=influxOrg)
     write_api = influxClient.write_api(write_options=SYNCHRONOUS)
     while True:
         (degF, rH) = readSensor(url, headers)
@@ -49,6 +55,7 @@ def main():
         ]
         write_api.write(bucket=influxBucket, record=record)
         time.sleep(sleepTime)
+
 
 if __name__ == "__main__":
     main()
