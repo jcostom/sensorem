6422350fbc69c7df849a372c853f705d96ccc319
Jason Costomiris <jcostom@jasons.org>
2022-04-15 14:47:23 -0400

    Updates to style - makes things more compliant with PEP8

--- sensorem.py
+++ sensorem.py
@@ -9,16 +9,16 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 APIKEY = os.getenv('APIKEY')
 DEVID = os.getenv('DEVID')
-sleepTime = int(os.getenv('sleepTime', 300))
-influxBucket = os.getenv('influxBucket')
-influxOrg = os.getenv('influxOrg')
-influxToken = os.getenv('influxToken')
-influxURL = os.getenv('influxURL')
-influxMeasurementName = os.getenv('influxMeasurementName')
+SLEEPTIME = int(os.getenv('SLEEPTIME', 300))
+INFLUX_BUCKET = os.getenv('INFLUX_BUCKET')
+INFLUX_ORG = os.getenv('INFLUX_ORG')
+INFLUX_TOKEN = os.getenv('INFLUX_TOKEN')
+INFLUX_URL = os.getenv('INFLUX_URL')
+INFLUX_MEASUREMENT_NAME = os.getenv('INFLUX_MEASUREMENT_NAME')
 DEBUG = int(os.getenv('DEBUG', 0))
 
-VER = '1.9'
-USER_AGENT = "/".join(['sensorem.py', VER])
+VER = '1.10'
+USER_AGENT = f"sensorem.py/{VER}"
 
 # Setup logger
 logger = logging.getLogger()
@@ -40,37 +40,33 @@ def c2f(celsius):
     return (celsius * 9/5) + 32
 
 
-def readSensor(sbURL, sbHeaders):
-    r = requests.get(sbURL, headers=sbHeaders)
-    # return array of (degF, rHum)
-    return (round(c2f(r.json()['body']['temperature']), 1),
-            r.json()['body']['humidity'])
+def read_sensor(switchbot_url, switchbot_headers):
+    r = requests.get(switchbot_url, headers=switchbot_headers)
+    # return array of (deg_f, rel_hum)
+    return [round(c2f(r.json()['body']['temperature']), 1),
+            r.json()['body']['humidity']]
 
 
 def main():
     logger.info(f"Startup: {USER_AGENT}")
-    url = "/".join(
-        ("https://api.switch-bot.com/v1.0/devices",
-         DEVID,
-         "status")
-    )
+    url = f"https://api.switch-bot.com/v1.0/devices/{DEVID}/status"
     headers = {'Authorization': APIKEY}
-    influxClient = InfluxDBClient(url=influxURL, token=influxToken,
-                                  org=influxOrg)
+    influxClient = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN,
+                                  org=INFLUX_ORG)
     write_api = influxClient.write_api(write_options=SYNCHRONOUS)
     while True:
-        (degF, rH) = readSensor(url, headers)
+        (deg_f, rel_hum) = read_sensor(url, headers)
         record = [
             {
-                "measurement": influxMeasurementName,
+                "measurement": INFLUX_MEASUREMENT_NAME,
                 "fields": {
-                    "degF": degF,
-                    "rH": rH
+                    "degF": deg_f,
+                    "rH": rel_hum
                 }
             }
         ]
-        write_api.write(bucket=influxBucket, record=record)
-        sleep(sleepTime)
+        write_api.write(bucket=INFLUX_BUCKET, record=record)
+        sleep(SLEEPTIME)
 
 
 if __name__ == "__main__":
